552844d3d407df5a2049eade8769055cb047939d
Septian Adi Wijaya <developadi@gmail.com>
2021-10-08 11:14:05 +0700

    setup footer

--- App.js
+++ App.js
@@ -1,43 +1,71 @@
-
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {
-  SafeAreaView, 
-  StyleSheet,
-  View, 
-  Text
-} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text, TextInput} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView style="{{flex:1, backgroundColor: mainColor.white}}">
-
+    <SafeAreaView style={mainStyles.mainContainer}>
       <View style={mainStyles.header}>
-        <Text style={{ fontWeight: 'bold', fontSize: 20, color: mainColors.primary}}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: 20,
+            color: mainColors.primary,
+          }}>
           TODO APP
         </Text>
-        <Icon name="delete" color="red" size={25}/>
+        <Icon name="delete" color="red" size={25} />
       </View>
-
+      <View style={mainStyles.footer}>
+        <View style={mainStyles.footer_container}>
+          <TextInput placeholder="Add Todo here ..."></TextInput>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
-
 //setup properties
 
 const mainColors = {
-  primary: '#1f145c'
-  , white: '#fff'
-}
+  primary: '#1f145c',
+  white: '#fff',
+};
 
 const mainStyles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: mainColors.white,
+  },
+
   header: {
-    padding: 20
-    , flexDirection: 'row'
-    , alignItems: 'center'
-    , justifyContent: 'space-between'
-  }
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  footer: {
+    position: 'absolute',
+    color: mainColors.white,
+    width: '100%',
+    bottom: 0,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  footer_container: {
+    backgroundColor: mainColors.white,
+    elevation: 40,
+    height: 50,
+    flex: 1,
+    marginVertical: 20,
+    marginRight: 20,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+  },
 });
 
 export default App;
