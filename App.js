347c083e5d5a189d775aea0ad057d34fca184c84
Septian Adi Wijaya <developadi@gmail.com>
2021-10-08 10:18:18 +0700

    setup header

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 
 import React from 'react';
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
-  SafeAreaView
-  , StyleSheet
+  SafeAreaView, 
+  StyleSheet,
+  View, 
+  Text
 } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style="{{flex:1, backgroundColor: mainColor.white}}">
+
+      <View style={mainStyles.header}>
+        <Text style={{ fontWeight: 'bold', fontSize: 20, color: mainColors.primary}}>
+          TODO APP
+        </Text>
+        <Icon name="delete" color="red" size={25}/>
+      </View>
 
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
 
+//setup properties
+
+const mainColors = {
+  primary: '#1f145c'
+  , white: '#fff'
+}
+
+const mainStyles = StyleSheet.create({
+  header: {
+    padding: 20
+    , flexDirection: 'row'
+    , alignItems: 'center'
+    , justifyContent: 'space-between'
+  }
 });
 
 export default App;
